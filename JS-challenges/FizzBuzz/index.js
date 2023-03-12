fadc5dcd41181a666bff2aea29ff04a38a14bcfb
enikma-mod <sirmodika@gmmail.com>
2023-03-12 15:03:25 +0200

    Fizzbuzz

---
+++ JS-challenges/FizzBuzz/index.js
@@ -0,0 +1,17 @@
+function fizzBuzz(startNum, endNum) {
+    for (let i = startNum; i <= endNum; i++) {
+        if (i % 15 == 0) {
+            console.log("FizzBuzz");
+        }
+        else if (i % 3 == 0) {
+            console.log("Fizz");
+        }
+        else if (i % 5 == 0) {
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+fizzBuzz(1, 100);
